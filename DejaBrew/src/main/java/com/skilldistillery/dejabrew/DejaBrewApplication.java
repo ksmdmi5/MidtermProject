e7d6109f105e7647500a7f2c8692b2c40ca45b14
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-09-12 14:20:21 -0600

    create spring boot project

---
+++ DejaBrew/src/main/java/com/skilldistillery/dejabrew/DejaBrewApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dejabrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DejaBrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DejaBrewApplication.class, args);
+	}
+
+}
